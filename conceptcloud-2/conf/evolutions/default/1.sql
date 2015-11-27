1af804d942c49fdd0ef9ae29bc7b8c944e2d2e8a
gauri145 <gaurinaik.145@gmail.com>
2015-11-27 09:27:39 -0500

    Added conceptcloud executable

---
+++ conceptcloud-2/conf/evolutions/default/1.sql
@@ -0,0 +1,22 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table lattice_model (
+  id                        integer auto_increment not null,
+  lattice                   varchar(255),
+  constraint pk_lattice_model primary key (id))
+;
+
+
+
+
+# --- !Downs
+
+SET FOREIGN_KEY_CHECKS=0;
+
+drop table lattice_model;
+
+SET FOREIGN_KEY_CHECKS=1;
+
